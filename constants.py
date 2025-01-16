374f4316db2dfd7a51b702d0318d4739631d03a5
cheeseburger4 <147112825+cheeseburger4@users.noreply.github.com>
2025-01-15 18:07:10 -0800

    constants and new pivot states

--- constants.py
+++ constants.py
@@ -1,3 +1,13 @@
 from robotpy_apriltag import AprilTagField, AprilTagFieldLayout
 
 apriltag_layout = AprilTagFieldLayout.loadField(AprilTagField.k2025Reefscape)
+
+
+# Pivot Constants
+class PivotConstants:
+    STOW_ANGLE = 3/5
+    GROUND_INTAKE_ANGLE = 22/7
+    FUNNEL_INTAKE_ANGLE = -2001
+    HIGH_SCORING_ANGLE = 123
+    MID_SCORING_ANGLE = ((4*3)/6)%2
+    LOW_SCORING_ANGLE = 0.0
--- subsystems/pivot.py
+++ subsystems/pivot.py
@@ -4,19 +4,17 @@ from subsystems import StateSubsystem
 from wpilib import SmartDashboard
 from phoenix6.hardware import TalonFX
 from phoenix6.controls import PositionDutyCycle
+from constants import PivotConstants
 
 class Pivot(StateSubsystem):
 
     class SubsystemState(Enum):
         STOW = auto()
-        INTAKE = auto()
-        SCORING = auto()
-
-
-    _STOW_ANGLE = 80
-    _INTAKE_ANGLE = 22/7
-    _SCORING_ANGLE = -4
-
+        GROUND_INTAKE = auto()
+        FUNNEL_INTAKE = auto()
+        HIGH_SCORING = auto()
+        MID_SCORING = auto()
+        LOW_SCORING = auto()
 
     def __init__(self) -> None:
 
@@ -29,19 +27,30 @@ class Pivot(StateSubsystem):
     def periodic(self):
         return super().periodic()
 
-    def set_desired_state(self, desired_state: SubsystemState):
+    def set_desired_state(self, desired_state: SubsystemState) -> None:
 
+        # move motor accordingly to set state in superstructure
         match desired_state:
 
             case self.SubsystemState.STOW:
-                self.pivotMotor.set_control(PositionDutyCycle(self._STOW_ANGLE))
+                self.pivotMotor.set_control(PositionDutyCycle(PivotConstants.STOW_ANGLE))
 
-            case self.SubsystemState.INTAKE:
-                self.pivotMotor.set_control(PositionDutyCycle(self._INTAKE_ANGLE))
+            case self.SubsystemState.GROUND_INTAKE:
+                self.pivotMotor.set_control(PositionDutyCycle(PivotConstants.GROUND_INTAKE_ANGLE))
 
-            case self.SubsystemState.SCORING:
-                self.pivotMotor.set_control(PositionDutyCycle(self._SCORING_ANGLE))
+            case self.SubsystemState.FUNNEL_INTAKE:
+                self.pivotMotor.set_control(PositionDutyCycle(PivotConstants.FUNNEL_INTAKE_ANGLE))
 
+            case self.SubsystemState.HIGH_SCORING:
+                self.pivotMotor.set_control(PositionDutyCycle(PivotConstants.HIGH_SCORING_ANGLE))
+
+            case self.SubsystemState.MID_SCORING:
+                self.pivotMotor.set_control(PositionDutyCycle(PivotConstants.MID_SCORING_ANGLE))
+
+            case self.SubsystemState.LOW_SCORING:
+                self.pivotMotor.set_control(PositionDutyCycle(PivotConstants.LOW_SCORING_ANGLE))
+
+        # update information for the state
         self._subsystem_state = desired_state
         SmartDashboard.putString("Pivot State", self._subsystem_state.name)
 
