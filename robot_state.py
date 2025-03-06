39ef3df33c5c626915bef0a58051b600859f2e87
Caden Dalley <caden.dalley@icloud.com>
2025-03-05 16:51:30 -0800

    Move Climber mechanism to climber.py

--- robot_state.py
+++ robot_state.py
@@ -1,7 +1,4 @@
 from commands2 import Subsystem
-from phoenix6 import utils
-from wpilib import SmartDashboard, Mechanism2d, Color8Bit
-from wpimath import units
 from wpimath.geometry import Pose2d
 
 from subsystems.climber import ClimberSubsystem
@@ -18,21 +15,3 @@ class RobotState(Subsystem):
         self._pivot = pivot
         self._elevator = elevator
         self._climber = climber
-
-        self._climber_mechanism = None
-        if utils.is_simulation():
-            self._setup_simulation_mechanisms()
-
-    def _setup_simulation_mechanisms(self):
-        self._climber_mechanism = Mechanism2d(1, 1)
-        self._climber_root = self._climber_mechanism.getRoot("Root", 1/2, 0)
-        self._climber_base = self._climber_root.appendLigament("Base", units.inchesToMeters(18.25), 90, 5, Color8Bit(194, 194, 194))
-        self._climber_arm = self._climber_base.appendLigament("Arm", units.inchesToMeters(9.424631), 0, 3, Color8Bit(100, 100, 100))
-        SmartDashboard.putData("Climber Mechanism", self._climber_mechanism)
-
-    def simulationPeriodic(self) -> None:
-        self.update_mechanisms()
-
-    def update_mechanisms(self) -> None:
-        if self._climber_mechanism:
-            self._climber_arm.setAngle(self._climber.get_position() * 360)
